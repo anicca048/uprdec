46d99d9cc70baf63c4ada44943b700451a34af4e
John Smith <anicca048@protonmail.com>
2020-11-10 05:10:22 -0600

    Code refactoring and cleanup.

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Uplink REDSHRT file decoder.
 Useful tool for (de)obfuscating Uplink REDSHRT files, to allow inspection and
 modification of game saves / data files.
 
-usage: uprdec.py \[-h\] \[-v\] \[-f\] mode input output.
+usage: uprdec.py \[-h\] \[-v\] \[-f\] mode input output
 
-This project is licensed under the terms of the MIT license.
+Note: you can not modify files in place.
+
+**This project is licensed under the terms of the MIT open source license.**
--- uprdec.py
+++ uprdec.py
@@ -25,12 +25,26 @@
 '''
  uprdec.py, Uplink REDSHRT decoder
 
- Python program to (de)obfuscate Uplink REDSHRT data files, does not support
- decompression of REDSHRT archive files (yet!).
+ Python program to (de)obfuscate Uplink REDSHRT data files, does not yet support
+ the decompression of REDSHRT archive files (just uses zip compression).
 
  File validity and program safeguards are based on how Uplink handles said
- checks, and are far from infaliable. If you try to break them you will 
+ checks, and are far from infallible. If you try to break them you will 
  probably be successful, so handle with care!
+'''
+'''
+ A note to the devs of Uplink:
+ 
+ To begin, I only bother to do this kind of thing for games that I love.
+ Uplink is one of my favorite games of all time, and I knew that eventually I
+ would start to peer under the hood. Plus, given the genre I think that it's
+ more than appropriate.
+ 
+ This project was really fun for me, it's rare for a savefile scheme to have any
+ protection mechanisms that provide some challenge. The mechanisms specifically
+ were simple, but well chosen, and match the spirit of the game beautifully.
+ 
+ Thanks :)
 '''
 
 import os
@@ -42,7 +56,7 @@ import hashlib
 try:
     assert sys.version_info >= (3, 0, 0), "python version error"
 except AssertionError:
-    print("error: python3 is required to run this program!")
+    print("Error: python3 is required to run this program!", file=sys.stderr)
     exit(1)
 
 # Two verisons of REDSHRT and v2 hash modified sha1 checksum.
@@ -54,220 +68,246 @@ HASH_SIZE = 20
 # Uplink uses 16kiB buffer for checksum and (de)obfuscation operations.
 BUFF_SIZE = 16384
 
-# Track header version across program.
-header_ver = 0
-# Original file hash before modification of data.
-orig_hash = b'\x00' * 20
-# Recalculated file hash for comparison / modification.
-new_hash = b'\x00' * 20
-
 # Make sure we are dealing with a REDSHRT file.
 def check_header(input_file):
-	global header_ver, orig_hash
-	
-	# Read in file magic number (first 9 bytes).
-	magic = input_file.read(MAGIC_SIZE)
-	
-	if len(magic) != MAGIC_SIZE:
-		return -1
-	
-	# Check magic nuber against known good values.
-	if magic == MAGIC_NUMS[0]:
-		header_ver = 1
-	elif magic == MAGIC_NUMS[1]:
-		header_ver = 2
-		# Version 2 files have a modified sha1 hash.
-		orig_hash = input_file.read(HASH_SIZE)
-		
-		if len(orig_hash) != HASH_SIZE:
-			return -1
-	else:
-		return -1
+    # Set empty header version (serves as an error code).
+    header_ver = None
+    # Original file hash before modification of data.
+    orig_hash = b'\x00' * 20
+    
+    # Read in file magic number (first 9 bytes).
+    magic = input_file.read(MAGIC_SIZE)
+    
+    if len(magic) != MAGIC_SIZE:
+        return (header_ver, orig_hash)
+    
+    # Check magic nuber against known good values.
+    if magic == MAGIC_NUMS[0]:
+        header_ver = 1
+    elif magic == MAGIC_NUMS[1]:
+        header_ver = 2
+        # Version 2 files have a modified sha1 hash.
+        orig_hash = input_file.read(HASH_SIZE)
+        
+        if len(orig_hash) != HASH_SIZE:
+            header_ver = None
+    
+    return (header_ver, orig_hash)
 
 # Generate checksum for REDSHRT data.
-def rehash(output_file):
-	global orig_hash, new_hash
-	
-	# Create hash context object.
-	sha1_ctxt = hashlib.sha1()
-	
-	# Load first chunk from file.
-	buff = output_file.read(BUFF_SIZE)
-	
-	# Hash chunks.
-	while buff:
-		sha1_ctxt.update(buff)
-		buff = output_file.read(BUFF_SIZE)
-	
-	# Create temporary hash to hold correct value while we do reordering.
-	tmp_hash = bytearray(sha1_ctxt.digest())
-	mod_hash = bytearray(b'\x00' * 20)
-	
-	# Must reorder correct hash to replicate modified sha1 used by Uplink.
-	for i in range(20):
-		mod_hash[i] = tmp_hash[(((i // 4) * 4) + (3 - (i % 4)))]
-	
-	# Save modified hash value.
-	new_hash = bytes(mod_hash)
+def gen_hash(output_file):
+    # Create hash context object.
+    sha1_ctxt = hashlib.sha1()
+    
+    # Load first chunk from file.
+    buff = output_file.read(BUFF_SIZE)
+    
+    # Hash chunks.
+    while buff:
+        sha1_ctxt.update(buff)
+        buff = output_file.read(BUFF_SIZE)
+    
+    # Create temporary hash to hold correct value while we do reordering.
+    tmp_hash = bytearray(sha1_ctxt.digest())
+    mod_hash = bytearray(b'\x00' * 20)
+    
+    '''
+     Software implementations of SHA1 on LittleEndian systems reorder the
+     final bytes of the hash to match the BigEndian used in the SHA1 spec.
+     The Uplink devs removed the reordering loop from their implementation, I
+     assume to throw people off the trail. :P
+     To match this behavior, we run my slick version of the reordering loop,
+     reversing the algo's reordering. :)
+    '''
+    for i in range(20):
+        mod_hash[i] = tmp_hash[(((i // 4) * 4) + (3 - (i % 4)))]
+    
+    # Save modified hash value.
+    new_hash = bytes(mod_hash)
+    
+    return new_hash
 
 # (De)obfuscate REDSHRT file data.
-def encode_decode(input_file, output_file):
-	global header_ver
-	
-	# Write header to file.
-	if header_ver == 1:
-		output_file.write(MAGIC_NUMS[0])
-	else:
-		output_file.write(MAGIC_NUMS[1])
-	
-	# Write empty hash section for version 2 files.
-	if header_ver == 2:
-		output_file.write(b'\x00' * 20)
-	
-	# Load first data chunk.
-	buff = bytearray(input_file.read(BUFF_SIZE))
-	
-	# (De)obfuscate data chunks.
-	while buff:
-		for i in range(len(buff)):
-			buff[i] ^= 128
-		
-		# Write (de)obfuscated input chunk to output.
-		output_file.write(bytes(buff))
-		# Load next chunk.
-		buff = bytearray(input_file.read(BUFF_SIZE))
+def encode_decode(input_file, output_file, header_ver):
+    # Write header to file.
+    if header_ver == 1:
+        output_file.write(MAGIC_NUMS[0])
+    else:
+        output_file.write(MAGIC_NUMS[1])
+    
+    # Write empty hash section for version 2 files.
+    if header_ver == 2:
+        output_file.write(b'\x00' * 20)
+    
+    # Load first data chunk.
+    buff = bytearray(input_file.read(BUFF_SIZE))
+    
+    '''
+     The main body of the REDSHRT files is obfuscated by adding 128
+     (10000000) to each byte, and deobfuscated by subtracting 128 from each
+     byte. This really just flips the 8th bit in each byte (relying on how
+     most systems handle overflow and underflow events). However, this can be
+     more simply achieved with xoring, making the operation the same for
+     obfuscation and deobfuscation.
+    '''
+    while buff:
+        for i in range(len(buff)):
+            buff[i] ^= 128
+        
+        # Write (de)obfuscated input chunk to output.
+        output_file.write(bytes(buff))
+        # Load next chunk.
+        buff = bytearray(input_file.read(BUFF_SIZE))
 
 # Entry point fuction, handles ui and file io.
 def main(operation, input_path, output_path, verbosity, safety_off):
-	# Make sure input file exists and is not empty.
-	if not os.path.isfile(input_path):
-		print("[!] Error:", input_path, "not found!")
-		exit(1)
+    # Make sure input file exists and is not empty.
+    if not os.path.isfile(input_path):
+        print("[!] Error:", input_path, "not found!", file=sys.stderr)
+        exit(1)
 
-	# Check if output file path is a directory, and if so use input file name.
-	if os.path.basename(output_path) == "":
-		output_path += os.path.basename(input_path)
-	
-	# Make sure input and output location are not the same.
-	if input_path == output_path:
-		print("[!] Error: cannot modify file in-place!")
-		exit(1)
-	
-	# Open input file for header check.
-	input_file = open(input_path, "rb")
-	
-	# Check header to make sure we have a valid input file.
-	if verbosity:
-		print("[+] Checking", input_path, "for REDSHRT header.")
-	
-	if check_header(input_file) == -1:
-		print("[!] Error:", input_path, "is not a REDSHRT file!")
-		input_file.close()
-		exit(1)
-	
-	# Print the current hash if verbose output was requested.
-	if header_ver == 2 and verbosity:
-		print("[#] Header hash:", orig_hash.hex())
-	
-	# If version 2 we need to check validity of file by comparing hashes.
-	if header_ver == 2:
-		if operation == "decode":
-			rehash(input_file)
-			
-			# If hashes don't match it is not considered a valid REDSHRT file.
-			if orig_hash != new_hash:
-				# Just warn if user used --force.
-				if safety_off:
-					print("[#] Warning:", input_path, "has incorrect hash!")
-					print("[#] File data may be corrupt or invalid!")
-				# Unrecoverable error otherwise.
-				else:
-					print("[!] Error:", input_path, "has incorrect hash!")
-					print("[!] File data may be corrupt or invalid!")
-					exit(1)
-			
-			# Reset read location for decode operation.
-			input_file.seek(MAGIC_SIZE + HASH_SIZE, 0)
-		elif operation == "encode":
-			# If hashes match assume that the file is already encoded.
-			if orig_hash != b'\x00' * 20:
-				if safety_off:
-					print("[#] Warning:", input_path, " is already encoded!")
-					print("[#] File must be decoded first!")
-				# Unrecoverable error otherwise.
-				else:
-					print("[!] Error:", input_path, " is already encoded!")
-					print("[!] File must be decoded first!")
-					exit(1)
-	
-	# Create / override output file for writing modified data.
-	output_file = open(output_path, "w+b")
-	
-	# Inform user of operation.
-	if operation == "encode":
-		print("[+] Writing encoded data to:", output_path)
-	elif operation == "decode":
-		print("[+] Writing decoded data to:", output_path)
-	
-	# Run obfuscate / deobfuscate function.
-	encode_decode(input_file, output_file)
-	
-	# Cleanup.
-	input_file.close()
-	output_file.close()
-	
-	# If an encode operation was requested, we need to rehash the file.
-	if operation == "encode" and header_ver == 2:
-		print("[+] Rehashing data in:", output_path)
-		# Reopen file in read-write mode for hashing.
-		output_file = open(output_path, "r+b")
-		# Seek past header to hash body.
-		output_file.seek(MAGIC_SIZE + HASH_SIZE, 0)
-		
-		# Run rehash operation on obfuscated data.
-		rehash(output_file)
-		
-		# Write new hash to file.
-		output_file.seek(MAGIC_SIZE, 0)
-		output_file.write(new_hash)
-		
-		# Final Cleanup.
-		output_file.close()
+    # Check if output file path is a directory, and if so use input file name.
+    if os.path.basename(output_path) == "":
+        output_path += os.path.basename(input_path)
+    
+    # Make sure input and output location are not the same.
+    if input_path == output_path:
+        print("[!] Error: cannot modify file in-place!", file=sys.stderr)
+        exit(1)
+    
+    # Open input file for header check.
+    input_file = open(input_path, "rb")
+    
+    # Check header to make sure we have a valid input file.
+    if verbosity:
+        print("[+] Checking", input_path, "for REDSHRT header.")
+    
+    header_ver, orig_hash = check_header(input_file)
+    
+    if not header_ver:
+        print("[!] Error:", input_path, "is not a REDSHRT file!",
+              file=sys.stderr)
+        input_file.close()
+        exit(1)
+    
+    # Print the current hash if verbose output was requested.
+    if header_ver == 2 and verbosity:
+        print("[#] Header hash:", orig_hash.hex())
+    
+    # Recalculated file hash for comparison / modification.
+    new_hash = b'\x00' * 20
+    
+    # If version 2 we need to check validity of file by comparing hashes.
+    if header_ver == 2:
+        if operation == "decode":
+            # Check for already decoded file.
+            if orig_hash == (b'\x00' * 20):
+                print("[#] Hash is zeroed: file might already be decoded!",
+                    file=sys.stderr)
+            
+            # Generate new hash.
+            new_hash = gen_hash(input_file)
+            
+            # If hashes don't match it is not considered a valid REDSHRT file.
+            if orig_hash != new_hash:
+                # Just warn if user used --force.
+                if safety_off:
+                    print("[#] Warning:", input_path, "has an incorrect hash!",
+                          file=sys.stderr)
+                    print("[#] File data may be corrupt or invalid!",
+                          file=sys.stderr)
+                # Unrecoverable error otherwise.
+                else:
+                    print("[!] Error:", input_path, "has an incorrect hash!",
+                          file=sys.stderr)
+                    print("[!] File data may be corrupt or invalid!",
+                          file=sys.stderr)
+                    exit(1)
+            
+            # Reset read location for decode operation.
+            input_file.seek(MAGIC_SIZE + HASH_SIZE, 0)
+        elif operation == "encode":
+            # If hashes match assume that the file is already encoded.
+            if orig_hash != b'\x00' * 20:
+                if safety_off:
+                    print("[#] Warning:", input_path, "has an incorrect hash!",
+                          file=sys.stderr)
+                    print("[#] File may already be encoded!", file=sys.stderr)
+                # Unrecoverable error otherwise.
+                else:
+                    print("[!] Error:", input_path, "has an incorrect hash!",
+                          file=sys.stderr)
+                    print("[!] File may already be encoded!", file=sys.stderr)
+                    exit(1)
+    
+    # Create / override output file for writing modified data.
+    output_file = open(output_path, "w+b")
+    
+    # Inform user of operation.
+    if operation == "encode":
+        print("[+] Writing encoded data to:", output_path)
+    elif operation == "decode":
+        print("[+] Writing decoded data to:", output_path)
+    
+    # Run obfuscate / deobfuscate function.
+    encode_decode(input_file, output_file, header_ver)
+    
+    # Cleanup.
+    input_file.close()
+    output_file.close()
+    
+    # If an encode operation was requested, we need to rehash the file.
+    if operation == "encode" and header_ver == 2:
+        print("[+] Rehashing data in:", output_path)
+        # Reopen file in read-write mode for hashing.
+        output_file = open(output_path, "r+b")
+        # Seek past header to hash body.
+        output_file.seek(MAGIC_SIZE + HASH_SIZE, 0)
+        
+        # Run rehash operation on obfuscated data.
+        new_hash = gen_hash(output_file)
+        
+        # Write new hash to file.
+        output_file.seek(MAGIC_SIZE, 0)
+        output_file.write(new_hash)
+        
+        # Final Cleanup.
+        output_file.close()
 
-		# Print the new hash if verbose output was requested.
-		if verbosity:
-			print("[#] New REDSHRT hash:", new_hash.hex())
-	
-	# Inform user of completion.
-	print("[+] done.")
+        # Print the new hash if verbose output was requested.
+        if verbosity:
+            print("[#] New REDSHRT hash:", new_hash.hex())
+    
+    # Inform user of completion.
+    print("[+] done.")
 
 # Entry point guard.
 if __name__ == "__main__":
-	# Setup parser for command line arguments.
-	parser = argparse.ArgumentParser(
-		prog="uprdec.py",
-		description="Tool to (de)obfuscate Uplink REDSHRT files.")
-	
-	# Add arguments to parser.
-	parser.add_argument("mode", help="mode of operation: < encode || decode >")
-	parser.add_argument("input", help="path to input file")
-	parser.add_argument("output", help="path to output file")
-	parser.add_argument("-v", "--verbose", action="store_true", 
-						help="use verbose output")
-	parser.add_argument("-f", "--force", action="store_true",
-						help="ignore checksum when checking for file validity")
-	
-	# Fetch arguments from sys.argv[].
-	args = parser.parse_args()
-	
-	# Check for valid mode of operation.
-	if args.mode != "encode" and args.mode != "decode":
-		parser.print_help(sys.stderr)
-		exit(1)
-	
-	# Start program with user arguments.
-	main(operation = args.mode, input_path = args.input,
-		 output_path = args.output,
-		 verbosity = True if args.verbose else False,
-		 safety_off = True if args.force else False)
+    # Setup parser for command line arguments.
+    parser = argparse.ArgumentParser(
+        prog="uprdec.py",
+        description="Tool to (de)obfuscate Uplink REDSHRT files.")
+    
+    # Add arguments to parser.
+    parser.add_argument("mode", help="mode of operation: < encode || decode >")
+    parser.add_argument("input", help="path to input file")
+    parser.add_argument("output", help="path to output file")
+    parser.add_argument("-v", "--verbose", action="store_true", 
+                        help="use verbose output")
+    parser.add_argument("-f", "--force", action="store_true",
+                        help="ignore checksum when checking for file validity")
+    
+    # Fetch arguments from sys.argv[].
+    args = parser.parse_args()
+    
+    # Check for valid mode of operation.
+    if args.mode != "encode" and args.mode != "decode":
+        parser.print_help(sys.stderr)
+        exit(1)
+    
+    # Start program with user arguments.
+    main(operation = args.mode, input_path = args.input,
+         output_path = args.output,
+         verbosity = True if args.verbose else False,
+         safety_off = True if args.force else False)
 
